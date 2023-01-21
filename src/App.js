f52b73f61bc8bd7faab4394a61497b026340c392
Demitriuss777 <demitriusss777@gmail.com>
2023-01-21 13:47:35 +0200

    les1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import {Simpson} from "./componets/Simpsons/Simpson";
+
 const App = () => {
-    return (
-        <div>
-            App lesson1
-        </div>
-    );
+ return (
+  <div>
+      {/*<div>*/}
+      {/*    <div> name Homer</div>*/}
+      {/*    <div> surname Simpson</div>*/}
+      {/*    <img src="https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png" alt='Homer'/>*/}
+      {/*</div>*/}
+      <Simpson name={'Marge'} surname={'Simpson'} image={'https://static.wikia.nocookie.net/simpsons/images/0/0b/Sexy_Santa_Marge.png'}/>
+      {/*<div>*/}
+      {/*    <div> name Marge</div>*/}
+      {/*    <div> surname Simpson</div>*/}
+      {/*    <img src='https://static.wikia.nocookie.net/simpsons/images/0/0b/Sexy_Santa_Marge.png' alt='Marge'/>*/}
+      {/*</div>*/}
+      <Simpson name={'Homer'} surname={'SSimp'} image={"https://static.wikia.nocookie.net/simpsons/images/5/5f/Ninja_Homer_Tapped_Out.png"}/>
+  </div>
+ );
 };
 
 export {App};
