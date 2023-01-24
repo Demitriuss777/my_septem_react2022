6d716f82f667745ac50b6b2576d1711311a47e66
Demitriuss777 <demitriusss777@gmail.com>
2023-01-24 17:16:16 +0200

    les

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import {Simpson} from "./componets/Simpsons/Simpson";
 
 const App = () => {
+    const simpsonss =[
+        {   id: 1,
+            name: 'Homer',
+            surname:'Simpson',
+            image: 'https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png'
+        },
+           {id: 2,
+            name: 'Marge',
+            surname:'Simpson',
+            image: 'https://static.wikia.nocookie.net/simpsons/images/0/0b/Sexy_Santa_Marge.png'
+    },
+        {id: 3,
+            name: 'Bart',
+            surname:'Simpson',
+            image: 'https://static.simpsonswiki.com/images/thumb/6/65/Bart_Simpson.png/200px-Bart_Simpson.png'
+        }
+
+    ]
  return (
   <div>
       {/*<div>*/}
@@ -8,13 +26,22 @@ const App = () => {
       {/*    <div> surname Simpson</div>*/}
       {/*    <img src="https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png" alt='Homer'/>*/}
       {/*</div>*/}
-      <Simpson name={'Marge'} surname={'Simpson'} image={'https://static.wikia.nocookie.net/simpsons/images/0/0b/Sexy_Santa_Marge.png'}/>
       {/*<div>*/}
       {/*    <div> name Marge</div>*/}
       {/*    <div> surname Simpson</div>*/}
       {/*    <img src='https://static.wikia.nocookie.net/simpsons/images/0/0b/Sexy_Santa_Marge.png' alt='Marge'/>*/}
       {/*</div>*/}
-      <Simpson name={'Homer'} surname={'SSimp'} image={"https://static.wikia.nocookie.net/simpsons/images/5/5f/Ninja_Homer_Tapped_Out.png"}/>
+
+
+
+     {/* <Simpson name={'Homer'} surname={'SSimp'} image={"https://static.wikia.nocookie.net/simpsons/images/5/5f/Ninja_Homer_Tapped_Out.png"}/>
+      <Simpson name={'Marge'} surname={'Simpson'} image={'https://static.wikia.nocookie.net/simpsons/images/0/0b/Sexy_Santa_Marge.png'}/>
+  */}
+
+
+      {simpsonss.map(simpson=> <Simpson key={simpson.id} name={simpson.name} surname={simpson.surname}
+      image={simpson.image}/>)}
+
   </div>
  );
 };
