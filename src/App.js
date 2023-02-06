d6fc2379b9acffa735193ef7b3182d1ab4055b93
Demitriuss777 <demitriusss777@gmail.com>
2023-02-06 19:22:33 +0200

    hom2 start

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import {Users} from "./components/Users";
+import {UsersDietails} from "./components/UsersDietails";
+import './App.css'
+import {getUser} from "./Service/getUser";
+import {useEffect, useState} from "react";
 const App = () => {
+    const [users,setUsers]= useState([])
+    useEffect( ()=>{
+        getUser()
+            .then( value=>setUsers(value))
+    }, [])
+    console.log(users)
+
+
     return (
-        <div>
-            App
+
+        <div className="app">
+         <div className="infoBlock">
+         <Users/>
+         </div>
+           <div className="divider"/>
+            <div className="infoBlock">
+            <UsersDietails/>
+            </div>
         </div>
     );
 };
